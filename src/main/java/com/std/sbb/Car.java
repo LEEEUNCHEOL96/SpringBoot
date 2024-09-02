4d41fd056b94660a2b3d55ebef5bbf5271552f8a
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-02 10:58:06 +0900

    lombok 사용

--- src/main/java/com/std/sbb/Car.java
+++ src/main/java/com/std/sbb/Car.java
@@ -1,46 +1,18 @@
 package com.std.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
+@Getter // LOMBOK 으로 한방에 정리가능
+@Setter // LOMBOK 으로 한방에 정리가능
 public class Car {
     private int id;
     private int speed;
     private String name;
     private List<Integer> ids;
-
-    Car(int id, int speed, String name, List<Integer> ids) {
-        this.id = id;
-        this.speed = speed;
-        this.name =name;
-        this.ids =ids;
-    }
-
-    public void setId(int id) {
-        this.id=id;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed=speed;
-    }
-
-    public void setName(String name) {
-        this.name= name;
-    }
-
-    public void setIds(ArrayList ids) {
-        this.ids=ids;
-    }
-
-    public void getId() {
-    }
-
-    public void getSpeed() {
-    }
-
-    public void getName() {
-    }
-
-    public void getIds() {
-    }
 }
