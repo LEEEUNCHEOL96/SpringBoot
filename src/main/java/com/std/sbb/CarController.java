caa206f7b730fc717fc8a6c9be2ff31d9311d66c
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-03 08:51:43 +0900

    car_practice

--- src/main/java/com/std/sbb/CarController.java
+++ src/main/java/com/std/sbb/CarController.java
@@ -8,26 +8,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.ArrayList;
 import java.util.List;
 
-@GetMapping("/car/addCar")
-@ResponseBody
-public String addCar(@RequestParam("name") String name,
-                     @RequestParam("madeBy") String madeBy) {
-    lastId++;
-    Car c = new Car(lastId, name, madeBy);
-
-    carList.add(c);
-
-    return String.format("%d 번 자동차가 추가되었습니다.", lastId);
-}
-
 @Controller
 public class CarController {
-    List<Car> carList;
-    private final int lastId;
+    int lastId;
+    List<Car> cars;
 
-    CarController() {
+    CarController(){
         lastId = 0;
-        carList = new ArrayList<>();
+        cars = new ArrayList<>();
     }
+}
 
+    @GetMapping("/car/addCar")
+    @ResponseBody
+    public String addCar (@RequestParam("name") String name ,
+                          @RequestParam ("madeBy") String madeBy ){
+
+
+
+        return String.format("%d 번 자동차가 추가되었습니다.",carList.getId());
+    }
 }
