fde4d62e024f492f18a436dd4478d6d5bd586825
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-03 10:30:04 +0900

    remove,modify 기능능 정리

--- src/main/java/com/std/sbb/PersonController.java
+++ src/main/java/com/std/sbb/PersonController.java
@@ -36,25 +36,63 @@ public class PersonController {
 
     @GetMapping("/person/remove")
     @ResponseBody
-    public String removePerson(int id) {
+    public String removePerson(@RequestParam("id") int id) {
+        // 삭제 방법_ver.1
+        Person removePerson = null;
+        for (Person person : people) {
+            if (person.getId() == id) {
+                removePerson = person;
+            }
+        }
+        if (removePerson == null) {
+            return String.format("%d번 사람이 존재하지 않습니다.", id);
+        } else {
+            people.remove(removePerson);
+        }
+
+        return id + "번째 사람이 삭제되었습니다.";
+        // return String.format ("%d번째 사람이 삭제되었습니다.",id);
+
+/*      // 삭제 방법_ver.2 == ver.1 의 축약
         boolean removed = people.removeIf(person -> person.getId() == id);
         if (!removed) {
             return id + "번 사람이 존재하지 않습니다.";
         }
-        return String.format("%d 번 사람이 삭제되었습니다.", id);
+        return String.format("%d 번 사람이 삭제되었습니다.", id); */
     }
-
 
     @GetMapping("/person/modify")
     @ResponseBody
-    public String modify(int id, String name, int age) {
+    public String modify(@RequestParam("id") int id,
+                         @RequestParam("name") String name,
+                         @RequestParam("age") int age) {
+        // 수정 방법_ver.1
+        Person findPerson = null;
+
+        for ( Person person : people){
+            if ( person.getId() == id){
+                findPerson = person;
+            }
+        }
+
+        if (findPerson == null){
+            return String.format("%d번 사람이 존재하지 않습니다.",id);
+        }else {
+            findPerson.setName(name);
+            findPerson.setAge(age);
+        }
+
+        return String.format("%d번 사람이 수정되었습니다.",id);
+
+
+/*        // 수정 방법_ver.2
         Person findPerson = people.stream().filter(p -> p.getId() == id).findFirst().orElse(null);
 
         if (findPerson == null) {
             return String.format("%d 번 사람이 존재하지 않습니다.", id);
         }
 
-        return String.format("%d 번 사람이 수정되었습니다.", id);
+        return String.format("%d 번 사람이 수정되었습니다.", id);*/
     }
 
     @GetMapping("person/people")
